8125fcdb94d8b9fbb6a5d2cf2a5755e91a36e204
bhatt098 <55306347+bhatt098@users.noreply.github.com>
2021-03-27 14:08:32 +0530

    Update config.inc.php

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -2,7 +2,7 @@
   session_start();
   $db_host = "remotemysql.com";
 $db_user = "Kv8nryJmg9";
-$db_password = "a5W6qR1aKt";
+$db_password = "gJqBviHhZh";
   $dbName = "Kv8nryJmg9";
  // session_start();
   $conn=mysqli_connect($db_host, $db_user, $db_password, $dbName);
